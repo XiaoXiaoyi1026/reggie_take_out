9d27cad54112734656b486a0e717f37682c1f2ee
XiaoXiaoyi <xiaoxiaoyi1026@gmail.com>
2022-09-26 15:39:28 +0800

    完成菜品分页功能

--- src/main/java/com/xiaoxiaoyi/reggie/controller/DishController.java
+++ src/main/java/com/xiaoxiaoyi/reggie/controller/DishController.java
@@ -1,15 +1,15 @@
 package com.xiaoxiaoyi.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xiaoxiaoyi.reggie.common.R;
 import com.xiaoxiaoyi.reggie.dto.DishDto;
+import com.xiaoxiaoyi.reggie.entity.Dish;
 import com.xiaoxiaoyi.reggie.service.DishFlavorService;
 import com.xiaoxiaoyi.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 菜品相关的接口
@@ -38,5 +38,28 @@ public class DishController {
         log.info(dishDto.toString());
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功！");
+    }
+
+    /**
+     * 菜品分页查询
+     *
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<Dish>> page(int page, int pageSize, String name) {
+        // 1. 创建一个Page对象
+        Page<Dish> dishPage = new Page<>(page, pageSize);
+        // 2. 创建查询QueryWrapper对象
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        // 3. 添加条件
+        queryWrapper.like(name != null, Dish::getName, name);
+        queryWrapper.orderByDesc(Dish::getCreateTime);
+        // 4. 执行查询
+        Page<Dish> res = dishService.page(dishPage, queryWrapper);
+        // 5. 返回
+        return R.success(res);
     }
 }
