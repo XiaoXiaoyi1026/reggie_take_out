f6b6aa23480ea4cabc56650c89f7513808238031
XiaoXiaoyi <xiaoxiaoyi1026@gmail.com>
2022-10-10 15:42:21 +0800

    完成了C端用户订单的分页查询功能

--- src/main/java/com/xiaoxiaoyi/reggie/controller/OrdersController.java
+++ src/main/java/com/xiaoxiaoyi/reggie/controller/OrdersController.java
@@ -1,14 +1,16 @@
 package com.xiaoxiaoyi.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xiaoxiaoyi.reggie.common.BaseContext;
 import com.xiaoxiaoyi.reggie.common.R;
 import com.xiaoxiaoyi.reggie.entity.Orders;
 import com.xiaoxiaoyi.reggie.service.OrdersService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author xiaoxiaoyi
@@ -35,5 +37,26 @@ public class OrdersController {
         ordersService.submitOrders(orders);
 
         return R.success("订单提交成功！");
+    }
+
+    /**
+     * 用户订单分页查询
+     *
+     * @param page     页数
+     * @param pageSize 页面大小
+     * @return 分页查询
+     */
+    @GetMapping("/userPage")
+    public R<Page<Orders>> getUserOrders(@RequestParam Integer page, @RequestParam Integer pageSize) {
+        Page<Orders> ordersPage = new Page<>(page,  pageSize);
+        Long userId = BaseContext.getCurrentId();
+        LambdaQueryWrapper<Orders> ordersLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        ordersLambdaQueryWrapper.eq(Orders::getUserId, userId);
+        ordersLambdaQueryWrapper.orderByDesc(Orders::getOrderTime).orderByDesc(Orders::getCheckoutTime);
+        // 按条件进行分页查询
+        // SQL: select * from orders where user_id = ? limit ?, ?
+        ordersPage = ordersService.page(ordersPage, ordersLambdaQueryWrapper);
+
+        return R.success(ordersPage);
     }
 }
