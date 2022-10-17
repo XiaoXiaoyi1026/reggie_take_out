4f48bf1a32d824b06b7f931a1699cec9c0e7387d
XiaoXiaoyi <9298460+masteryiii@user.noreply.gitee.com>
2022-10-17 14:03:04 +0800

    redis环境相关配置

---
+++ src/main/java/com/xiaoxiaoyi/reggie/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.xiaoxiaoyi.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author xiaoxiaoyi
+ * redis配置类
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        // 设置key序列化器，JDK默认的序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        // 设置redis连接工厂
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        return redisTemplate;
+    }
+
+}
